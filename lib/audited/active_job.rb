f048113ae878124a30c82b740388ccdc654ccfb1
Mark Rebec <mark@markrebec.com>
2016-04-30 10:02:30 -0700

    extracts argument parsing for easier testing

--- lib/audited/active_job.rb
+++ lib/audited/active_job.rb
@@ -4,13 +4,17 @@ module Audited
       base.send :attr_reader, :audit_user
 
       base.send :around_perform do |_job, block|
-        options = arguments.extract_options!
-        @audit_user = options.delete(:audit_user)
-        arguments << options unless options.empty?
-        
+        extract_audit_user!
+
         Audited.audit_class.as_user(audit_user) do
           block.call
         end
+      end
+
+      def extract_audit_user!
+        options = arguments.extract_options!
+        @audit_user = options.delete(:audit_user)
+        arguments << options unless options.empty?
       end
     end
   end
